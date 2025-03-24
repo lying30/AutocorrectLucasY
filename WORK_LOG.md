ab2d36b07385f21cfa72b85bd9af6c77e76eecf2
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2025-03-24 16:46:01 -0700

    commit

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
-Name: YOUR NAME HERE
+Name: Lucas Ying
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date     |    Time     |                                                                                                                                                                                                                                                                               Update |
+|:---------|:-----------:|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| March 11 | 8:45am-10am |                                                                                                                                                                                                                    Thought through the problem as it was the first day of work time. |
+| march 20 | 8:45am-10am |                                                                                                                                                                                                                                      I did my edit distance method using tabulation. |
+| march 21 |  1:45-3pm   | I guess I had an error with my edit distance method and it was returning 0 for all cases, so I used the period to fix that bug and I got the tabulation working. Then, Clifford helped me with the scanner thing and so i started to try and make the main method and ask for input. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * Autocorrect
@@ -8,17 +10,31 @@ import java.io.IOException;
  * A command-line tool to suggest similar words when given one not in the dictionary.
  * </p>
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Lucas Ying
  */
 public class Autocorrect {
 
+    public static void main(String[] args) {
+        Autocorrect autocorrect = new Autocorrect(words, 3);
+        Scanner s = new Scanner(System.in);
+        while(true) {
+            System.out.print("Enter a word: ");
+            String typed = s.nextLine();
+            ArrayList<String> toReturn = new ArrayList<>();
+        }
+    }
+
+    private final String[] dictionaryWords;
+    private final int threshold;
+
     /**
-     * Constucts an instance of the Autocorrect class.
+     * Constructs an instance of the Autocorrect class.
      * @param words The dictionary of acceptable words.
      * @param threshold The maximum number of edits a suggestion can have.
      */
     public Autocorrect(String[] words, int threshold) {
-
+        this.dictionaryWords = words;
+        this.threshold = threshold;
     }
 
     /**
@@ -28,8 +44,48 @@ public class Autocorrect {
      * to threshold, sorted by edit distnace, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
+        ArrayList<String> matches = new ArrayList<>();
 
-        return new String[0];
+        for (String word : dictionaryWords) {
+            int distance = ed(typed, word);
+
+            if (distance <= threshold) {
+                matches.add(word);
+            }
+        }
+        matches.sort(Comparator.comparingInt((String word) -> ed(typed, word)).thenComparing(String::compareTo));
+
+        return matches.toArray(new String[0]);
+
+    }
+
+    public int ed(String typed, String word) {
+        if (word.isEmpty()) return -1;
+
+        int ed = 0;
+
+        int length1 = typed.length();
+        int length2 = word.length();
+        int[][] matrix = new int[length1 + 1][length2 + 1];
+
+        for (int j = 0; j <= length1 ; j++) {
+            matrix[j][0] = j;
+        }
+        for (int k = 0; k <= length2; k++) {
+            matrix[0][k] = k;
+        }
+
+        for (int j = 1; j <= length1; j++) {
+            for (int k = 1; k <= length2; k++) {
+                if (typed.charAt(j-1) == word.charAt(k-1)){
+                    matrix[j][k] = matrix[j-1][k-1];
+                } else {
+                    matrix[j][k] = 1 + Math.min(Math.min(matrix[j-1][k], matrix[j][k-1]), matrix[j-1][k-1]);
+                }
+            }
+        }
+        ed = matrix[length1][length2];
+        return ed;
     }
 
 
